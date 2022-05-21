b7f6c3bc47aa46dd7553ec6e0b0f5e3de861a7ab
ahmadmaen <ahmadmaen15@gmail.com>
2022-05-22 00:41:12 +0300

    version 2.1.0 5/22/2022

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,17 @@ import 'package:autism_helper_project/screens/Sign_in/Sign_in_Page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
