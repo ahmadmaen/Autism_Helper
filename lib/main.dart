c082cac97a1b8dece7bedb29436f50229b236982
ahmadmaen <ahmadmaen15@gmail.com>
2022-05-27 22:31:52 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,6 @@ import 'constants.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'Services/Auth.dart';
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'constants.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
