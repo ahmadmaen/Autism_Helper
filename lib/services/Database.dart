e2e23f874e5238543277a046cffbdef7ba63bc93
ahmadmaen <ahmadmaen15@gmail.com>
2022-06-04 12:52:18 +0300

    version 5.3.0 6/4/2022

--- lib/services/Database.dart
+++ lib/services/Database.dart
@@ -9,13 +9,14 @@ import 'api_path.dart';
 import 'firestore_service.dart';
 
 abstract class Database {
+
   Future<void> setUserData(User1 user);
 
   Stream<dynamic> readAlbums();
+  DocumentReference<Map<String, dynamic>> getUser();
 
-  FutureBuilder<DocumentSnapshot> getUser();
+
 }
-
 class FirestoreDatabase implements Database {
   FirestoreDatabase({required this.uid}) : assert(uid != null);
 
@@ -55,26 +56,8 @@ class FirestoreDatabase implements Database {
       );
 
   @override
-  FutureBuilder<DocumentSnapshot> getUser() {
-    CollectionReference users = FirebaseFirestore.instance.collection('/User');
-    return FutureBuilder<DocumentSnapshot>(
-        future: users.doc(uid).get(),
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          print("loading");
-          if (snapshot.hasError) {
-            print("Something went wrong");
-          }
-          if (snapshot.hasData && !snapshot.data!.exists) {
-            print("Document does not exist");
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            Map<String, dynamic> data =
-                snapshot.data!.data() as Map<String, dynamic>;
-            print("Full Name: ${data['full_name']} ${data['last_name']}");
-          }
-          print("loading");
-          return const Text("loading");
-        });
-  }
+  DocumentReference<Map<String, dynamic>> getUser() =>  _service.getUser(
+      path: APIPath.users(),
+      documentID: uid!
+  );
 }
