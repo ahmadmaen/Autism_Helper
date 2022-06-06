5b2269fa329f3010221df223e914ca1c63c49a36
ahmadmaen <ahmadmaen15@gmail.com>
2022-06-06 15:47:54 +0300

    z

---
+++ lib/services/Translator.dart
@@ -0,0 +1,18 @@
+
+
+import '../models/picture.dart';
+
+class Translator {
+  final int firstAlbumId = -1;
+  final int secondAlbumId = -1;
+  final String sentence = "";
+
+
+  void addPicture(Picture picture) {
+    if(firstAlbumId == -1)
+      {
+
+      }
+  }
+
+}
